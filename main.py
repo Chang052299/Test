6f404b10c4f274ae01025b7b86c3e16ec47b00ce
Chung-Yuan Chang <changchungyuan123@gmail.com>
2024-02-06 19:55:34 +0800

    test

---
+++ main.py
@@ -0,0 +1,16 @@
+class Person():
+	def __init__(self):
+		self.name = "John"
+	def display(self):
+		print("My name is: ", self.name)
+
+class Child(Person):
+	def __init__(self):
+		super().__init__()
+
+	def display2(self):
+		super().display()
+		print("test")
+
+test = Child()
+test.display2()
